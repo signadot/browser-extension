8545da52e91d1bc895a45bd3103e4bc63fa81570
Anirudh Ramanathan <anirudh@signadot.com>
2023-11-22 15:04:11 -0800

    add exception

--- src/background.ts
+++ src/background.ts
@@ -62,6 +62,7 @@ const getRules = (
               },
               condition: {
                 urlFilter: "*",
+                excludeUrlPatterns: ["*://*.preview.signadot.com/*"],
                 resourceTypes: MODIFY_HEADER_IN_RESOURCE_TYPES,
               },
             } as chrome.declarativeNetRequest.Rule)
