0c55f89e98b5402b83c238d3e476f3c4690e93fb
Anirudh Ramanathan <anirudh@signadot.com>
2025-04-30 14:40:00 -0700

    update visual styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,44 +1,41 @@
 import React from "react";
-import { AnchorButton, Section, SectionCard } from "@blueprintjs/core";
-import { FaGithub, FaSlack } from "react-icons/fa";
+import { FaGithub, FaSlack, FaBook } from "react-icons/fa";
 import styles from "./Footer.module.css";
 
 interface Props {}
 
 const Footer: React.FC<Props> = () => (
-  <Section icon="document" className={styles.container}>
-    <SectionCard>
-      <div className={styles.links}>
-        <AnchorButton
-          href="https://signadotcommunity.slack.com/join/shared_invite/zt-1estxm8pv-qfiaNfiFFCaW~eUlXsVoEQ"
-          target="_blank"
-          minimal
-          icon={<FaSlack />}
-          rel="noopener noreferrer" // Best practice for opening links in new tabs
-        >
-          Slack
-        </AnchorButton>
-        <AnchorButton
-          href="https://github.com/signadot"
-          target="_blank"
-          minimal
-          icon={<FaGithub />}
-          rel="noopener noreferrer" // Best practice for opening links in new tabs
-        >
-          GitHub
-        </AnchorButton>
-        <AnchorButton
-          href="https://www.signadot.com/docs/"
-          target="_blank"
-          minimal
-          icon="document"
-          rel="noopener noreferrer" // Best practice for opening links in new tabs
-        >
-          Documentation
-        </AnchorButton>
-      </div>
-    </SectionCard>
-  </Section>
+  <footer className={styles.container}>
+    <div className={styles.links}>
+      <a
+        href="https://signadotcommunity.slack.com/join/shared_invite/zt-1estxm8pv-qfiaNfiFFCaW~eUlXsVoEQ"
+        target="_blank"
+        className={styles.link}
+        rel="noopener noreferrer"
+      >
+        <FaSlack />
+        <span>Slack</span>
+      </a>
+      <a
+        href="https://github.com/signadot"
+        target="_blank"
+        className={styles.link}
+        rel="noopener noreferrer"
+      >
+        <FaGithub />
+        <span>GitHub</span>
+      </a>
+      <a
+        href="https://www.signadot.com/docs/"
+        target="_blank"
+        className={styles.link}
+        rel="noopener noreferrer"
+      >
+        <FaBook />
+        <span>Documentation</span>
+      </a>
+    </div>
+  </footer>
 );
 
 export default Footer;
